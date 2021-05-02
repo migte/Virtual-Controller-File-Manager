6b9674bcffcedfade81afcd3a52f07830abc9e84
migte <migtechannel@gmail.com>
2021-05-02 13:34:29 -0400

    Initial Commit

---
+++ Virtual Controller File Manager/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Virtual_Controller_File_Manager
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+
+	
+
+
+	public partial class MainWindow : Window
+    {
+		// Initializes window
+		public MainWindow()
+        {
+            InitializeComponent();
+        }
+		// Close Window
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+		// Minimize Window
+        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+		}
+		// Maximize Window
+        private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+				this.MoveBar.Tag = "Norm";
+			}
+			else
+            {
+                this.WindowState = WindowState.Maximized;
+				this.MoveBar.Tag = "Fullscreen";
+			}
+		}
+		// Move Window
+		private void MoveWindow(object sender, MouseButtonEventArgs e)
+		{
+			string windowState = this.MoveBar.Tag.ToString();
+			var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+			if (windowState == "Fullscreen" || this.WindowState.ToString() == "Maximized")
+            {
+				if (e.ChangedButton == MouseButton.Left)
+				{
+					Point pointToWindow = Mouse.GetPosition(this);
+					Point pointToScreen = PointToScreen(pointToWindow);
+                    var screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+                    this.WindowState = WindowState.Normal;
+					this.Left = pointToScreen.X - (screenWidth - this.Width * 2);
+					this.Top = pointToScreen.Y;
+					this.DragMove();
+				}
+				this.MoveBar.Tag = "Norm";
+			}
+			else if(e.ChangedButton == MouseButton.Left)
+            {
+				this.DragMove();
+
+			}
+
+		}
+
+
+        // Code I found online that makes sure the widnow maximizes properly
+        protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+			((HwndSource)PresentationSource.FromVisual(this)).AddHook(HookProc);
+		}
+
+		public static IntPtr HookProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			if (msg == WM_GETMINMAXINFO)
+			{
+				// We need to tell the system what our size should be when maximized. Otherwise it will cover the whole screen,
+				// including the task bar.
+				MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+				// Adjust the maximized size and position to fit the work area of the correct monitor
+				IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+				if (monitor != IntPtr.Zero)
+				{
+					MONITORINFO monitorInfo = new MONITORINFO();
+					monitorInfo.cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+					GetMonitorInfo(monitor, ref monitorInfo);
+					RECT rcWorkArea = monitorInfo.rcWork;
+					RECT rcMonitorArea = monitorInfo.rcMonitor;
+					mmi.ptMaxPosition.X = Math.Abs(rcWorkArea.Left - rcMonitorArea.Left);
+					mmi.ptMaxPosition.Y = Math.Abs(rcWorkArea.Top - rcMonitorArea.Top);
+					mmi.ptMaxSize.X = Math.Abs(rcWorkArea.Right - rcWorkArea.Left);
+					mmi.ptMaxSize.Y = Math.Abs(rcWorkArea.Bottom - rcWorkArea.Top);
+				}
+
+				Marshal.StructureToPtr(mmi, lParam, true);
+			}
+
+			return IntPtr.Zero;
+		}
+
+		private const int WM_GETMINMAXINFO = 0x0024;
+
+		private const uint MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+        public Uri Source { get; private set; }
+
+        [DllImport("user32.dll")]
+		private static extern IntPtr MonitorFromWindow(IntPtr handle, uint flags);
+
+		[DllImport("user32.dll")]
+		private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+		[Serializable]
+		[StructLayout(LayoutKind.Sequential)]
+		public struct RECT
+		{
+			public int Left;
+			public int Top;
+			public int Right;
+			public int Bottom;
+
+			public RECT(int left, int top, int right, int bottom)
+			{
+				this.Left = left;
+				this.Top = top;
+				this.Right = right;
+				this.Bottom = bottom;
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct MONITORINFO
+		{
+			public int cbSize;
+			public RECT rcMonitor;
+			public RECT rcWork;
+			public uint dwFlags;
+		}
+
+		[Serializable]
+		[StructLayout(LayoutKind.Sequential)]
+		public struct POINT
+		{
+			public int X;
+			public int Y;
+
+			public POINT(int x, int y)
+			{
+				this.X = x;
+				this.Y = y;
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct MINMAXINFO
+		{
+			public POINT ptReserved;
+			public POINT ptMaxSize;
+			public POINT ptMaxPosition;
+			public POINT ptMinTrackSize;
+			public POINT ptMaxTrackSize;
+		}
+	}
+}
